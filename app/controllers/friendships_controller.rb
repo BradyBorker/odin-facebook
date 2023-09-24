4dc0385c2d36068e3254fb00e1745b5b8a7a3d0d
Brady Barker <brady.d.barker@gmail.com>
2023-09-24 00:55:36 -0400

    Updated create action for friendships_controller Can build a friendship using the current_user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,15 @@
 class FriendshipsController < ApplicationController
     def create
+        @friendship = current_user.friendships.build(friend_id: params[:friend_id], pending: true)
+        
+        if @friendship
+            @friendship.save
+            flash[:notice] = 'Invitation Sent'
+        else
+            flash[:alert] = 'Invitation Failed To Send'
+        end
+
+        redirect_back(fallback_location: root_path)
     end
 
     def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.delete_all
+
+emails = ['test1@gmail.com', 'test2@gmail.com', 'test3@gmail.com', 'test4@gmail.com']
+emails.each do |email|
+    user = User.new(email: email)
+    user.save
+end
