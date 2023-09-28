3cf9b59a4493665ae1406091557b6939f1edf29d
Brady Barker <brady.d.barker@gmail.com>
2023-09-28 12:46:49 -0400

    Created create action for friend_invitations

--- app/controllers/friend_invitations_controller.rb
+++ app/controllers/friend_invitations_controller.rb
@@ -3,6 +3,15 @@ class FriendInvitationsController < ApplicationController
     end
     
     def create
+        @invitation = current_user.sent_invitations.build(receiver_id: params[:recipient])
+
+        if @invitation.save
+            flash[:notice] = "Invitation Successfully Sent"
+            redirect_back(fallback_location: root_path)
+        else
+            flash[:alert] = "Invitation Failed to Sent"
+            render :new
+        end
     end
 
     def destroy
