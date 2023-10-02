4917b6e295b19ff88900442d2c4cc0a83a806b9e
Brady Barker <brady.d.barker@gmail.com>
2023-10-01 21:03:26 -0400

    Created logic to like a post Next need to add number next to likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,18 @@
 class LikesController < ApplicationController
     def create
+        @post = Post.find(params[:post_id])
+
+        if @post.likes.where(user_id: current_user.id).any?
+            destroy
+        else
+            @like = @post.likes.create(user_id: current_user.id)
+            redirect_to root_path
+        end
     end
 
     def destroy
+        @like = @post.likes.find_by(user_id: current_user.id)
+        @like.destroy
+        redirect_to root_path
     end
 end
