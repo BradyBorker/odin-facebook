b60b59c5b73dc28bda14ac61c6e9cd183d24fb87
Brady Barker <brady.d.barker@gmail.com>
2023-10-07 18:52:44 -0400

    Added logic for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,26 @@ class User < ApplicationRecord
 
   scope :not_friends, ->(current_user) { where.not(id: Friendship.where(user_id: current_user).uniq.pluck(:friend_id).push(current_user)) }
 
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.user_information.first_name = auth.info.first_name
+      user.user_information.last_name = auth.info.last_name
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
   def friend_invitations_count
     received_invitations.count.to_s
   end
