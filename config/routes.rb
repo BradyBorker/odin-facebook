a85884616bd48180dea4460662f59abff80e65fb
Brady Barker <brady.d.barker@gmail.com>
2023-10-07 23:05:21 -0400

    Email is sent when a new user registers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'my_registrations' }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
