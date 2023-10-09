a782fab908890385f34e973f634ffe3ef657ce94
Brady Barker <brady.d.barker@gmail.com>
2023-10-08 20:22:51 -0400

    Email now sends upon user registration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
-    default from: 'notifications@example.com'
+    default from: 'endersprodigy@gmail.com'
 
     def new_registration
         @user = params[:user]
+        @user_name = @user.user_information.first_name
         @url = root_url
 
         mail(to: @user.email, subject: 'Welcome to My Site!')
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,10 +41,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #  address: 'smtp.gmail.com',
+  #  port: 587,
+  #  domain: 'example.com',
+  #  user_name: ENV['gmail_user_name'],
+  #  password: ENV['gmail_password'],
+  #  authentication: 'plain',
+  #  enable_starttls: true,
+  #  open_timeout: 5,
+  #  read_timeout: 5
+  #}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
