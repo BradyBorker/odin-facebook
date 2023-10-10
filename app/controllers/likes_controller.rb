281281b2e13e1b3d96c56b4018d7ba3e9f08d2e3
Brady Barker <brady.d.barker@gmail.com>
2023-10-10 19:08:26 -0400

    Bug with liking which wouldn't allow you to like all the posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,13 +6,13 @@ class LikesController < ApplicationController
             destroy
         else
             @like = @post.likes.create(user_id: current_user.id)
-            redirect_to root_path
+            redirect_back fallback_location: root_path
         end
     end
 
     def destroy
         @like = @post.likes.find_by(user_id: current_user.id)
         @like.destroy
-        redirect_to root_path
+        redirect_back fallback_location: root_path
     end
 end
