b6d17ff89072686a1166d6372777d18198cb85f5
Brady Barker <brady.d.barker@gmail.com>
2023-10-10 22:16:20 -0400

    Can now like a post and the page doesn't shoot to the top

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,31 @@
 class LikesController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
+        puts "MY POST HERE"
+        puts @post
 
         if @post.likes.where(user_id: current_user.id).any?
             destroy
         else
             @like = @post.likes.create(user_id: current_user.id)
-            redirect_back fallback_location: root_path
+
+            update_counter_text
         end
     end
 
     def destroy
         @like = @post.likes.find_by(user_id: current_user.id)
         @like.destroy
-        redirect_back fallback_location: root_path
+
+        update_counter_text
+    end
+
+    private
+
+    def update_counter_text
+        render turbo_stream:
+            turbo_stream.replace("counter_#{@post.id}",
+                partial: 'likes/like_counter',
+                locals: { post: @post })
     end
 end
