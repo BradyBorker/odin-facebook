a8ba4def92139554d43727737395bb7efe6629a7
Brady Barker <brady.d.barker@gmail.com>
2023-10-12 21:22:08 -0400

    Cleaned up likes_controller and _post partial Renamed _update_counter_text to _update_like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,35 +1,23 @@
 class LikesController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
-
-        # if @post.likes.where(user_id: current_user.id).any?
-        #    destroy
-        # else
-        #    @like = @post.likes.create(user_id: current_user.id)
-        #    update_counter_text
-        # end
-
-        update_counter_text if @post.likes.create(user_id: current_user.id)
+        update_like if @post.likes.create(user_id: current_user.id)
     end
 
     def destroy
-        # @like = @post.likes.find_by(user_id: current_user.id)
-        # @like.destroy
-        # update_counter_text
-
         @like = Like.find_by(post_id: params[:post_id], user_id: params[:id])
         @like.destroy
-        update_counter_text
+        update_like
     end
 
     private
 
-    def update_counter_text
+    def update_like
         @post ||= Post.find(params[:post_id])
 
         render turbo_stream:
-            turbo_stream.replace("counter_#{@post.id}",
-                partial: 'likes/like_counter',
+            turbo_stream.replace("like_#{@post.id}",
+                partial: 'likes/like',
                 locals: { post: @post })
     end
 end
