5efd7f55496739daa3f7291ef3b6f264cdcf7711
Brady Barker <brady.d.barker@gmail.com>
2023-10-19 19:50:15 -0400

    Added logic in my_registrations_controller to automatically make people my friend

--- app/controllers/my_registrations_controller.rb
+++ app/controllers/my_registrations_controller.rb
@@ -4,5 +4,8 @@ class MyRegistrationsController < Devise::RegistrationsController
         if @user.persisted?
             UserMailer.with(user: @user).new_registration.deliver_later
         end
+
+        Friendship.create(user_id: 1, friend_id: current_user.id)
+        Friendship.create(user_id: current_user.id, friend_id: 1)
     end
 end
