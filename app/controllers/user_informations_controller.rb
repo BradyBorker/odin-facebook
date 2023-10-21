5479a0bd2c9180f39db169a28d350f1359736359
Brady Barker <brady.d.barker@gmail.com>
2023-10-20 23:12:08 -0400

    Updated edit and update to update to UserInformation#update instead of User#update

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -1,8 +1,21 @@
 class UserInformationsController < ApplicationController
     def edit
-        @user = User.includes(:user_information).find(params[:user_id])
+        @user_information = UserInformation.find_by(user_id: current_user.id)
     end
 
     def update
+        @user_information = UserInformation.find(params[:ui_id])
+        
+        if @user_information.update(user_information_params)
+            redirect_back fallback_location: root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_information_params
+        params.require(:user_information).permit(:first_name, :last_name)
     end
 end
